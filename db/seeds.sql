12005c776031c796a6d8a6099a66fc06be91c21c
Jacqueline Young <jackiey1966@gmail.com>
2017-05-12 11:55:17 -0400

    progression

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devour) VALUES ('portibello' FALSE);
+INSERT INTO burgers (burger_name, devour) VALUES ('mushroom', FALSE);
+INSERT INTO burgers (burger_name, devour) VALUES ('bacon', FALSE);
